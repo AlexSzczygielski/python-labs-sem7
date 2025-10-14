f919d57c298586143d847cc1561ecedf66f6a92e
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-10-14 17:02:29 +0200

    task2

--- lab1/ex2.py
+++ lab1/ex2.py
@@ -1,28 +1,39 @@
 import random
-from multiprocessing import Process
+from multiprocessing import Pool, cpu_count
+import heapq
 
-def random_data(x):
+def get_random_data(x):
     data = []
     for i in range(x):
         data.append(random.randint(-x,x))
     return data
 
-def sort(data):
-    sorted_data = sorted(data)
-    return sorted_data
+def sort_single(data):
+    return sorted(data)
 
 def parallel_sort(data):
-    #Set number of parallel processes
-    #p = Pool(4)
-    #sorted_data = p.map(sort(data),len(data))
-    p = Process(target=sort, args=data)
-    p.start()
-    p.join()
+    #Establish the number of parallel processes
+    processes_count = cpu_count()
+    chunk_size = len(data) // processes_count #divide with int precision
+
+    #Divide dataset into smaller chunks, with chunk_size step
+    chunks = []
+    for i in range(0, len(data), chunk_size):
+        chunks.append(data[i : i+chunk_size])
     
-    #return sorted_data
+    #Parallel sorting
+    with Pool(processes_count) as p:
+        sorted_chunks = p.map(sort_single,chunks)
+
+    #Merge sorted data
+    #merged = sorted(sum(sorted_chunks, []))
+    merged = list(heapq.merge(*sorted_chunks))
+
+    return merged
+
 
 if __name__ == '__main__':
-    rand_data = random_data(100000)
+    rand_data = get_random_data(100000)
     print("RANDOM ",rand_data)
 
     sorted_data = parallel_sort(rand_data)
