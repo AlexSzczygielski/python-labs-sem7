f4900bb7b27336098a53c966b31e5ff5d2c979bd
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-10-28 17:34:36 +0100

    update lab3

--- lab3/ex1.py
+++ lab3/ex1.py
@@ -1,7 +1,15 @@
 import os
 
 if __name__ == "__main__":
-    dir_path = "/dev"
-    ls = os.listdir(dir_path)
-    print(f"Number of files and folders in {dir_path} directory: ", len(ls))
+    dir_path = "/Users/olek_szczygielski/Desktop/AGH/7_sem/python_programming/python-labs-sem7/lab3"
+    #dir_path = "/dev"
+    #ls = os.scandir(dir_path)
+    #print(f"Number of files and folders in {dir_path} directory: ", len(ls))
     #print(ls)
+    ls = os.scandir(dir_path)
+    entries = []
+    for entry in ls:
+        if not entry.name.startswith('.'):
+            entries.append(entry.name)
+            print(entry.name)
+print(len(entries))
